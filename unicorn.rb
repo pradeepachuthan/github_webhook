6230548da2f677603f198345be0caebefcafc986
Pradeep Achuthan <pradeep.achuthan@gmail.com>
2015-12-15 11:39:40 +0530

    changed path name

--- unicorn.rb
+++ unicorn.rb
@@ -1,5 +1,5 @@
 # define paths and filenames
-deploy_to = "/var/www/demo"
+deploy_to = "/var/www/"
 rails_root = "#{deploy_to}/current"
 pid_file = "#{deploy_to}/shared/pids/unicorn.pid"
 socket_file= "#{deploy_to}/shared/unicorn.sock"
