22c8c8117ffe6beda3b9b410b2ac2ea4428b4c54
Pradeep Achuthan <pradeep.achuthan@gmail.com>
2015-12-16 12:53:09 +0530

    added puts

--- web_hook_api.rb
+++ web_hook_api.rb
@@ -9,6 +9,7 @@ SETTINGS ||= YAML.load_file(File.join(Dir.pwd, 'config/config.yml'))
 class AutoDeployment <  Sinatra::Application
   
   def self.update(json_params)
+    puts "Inside update"
     request_payload = JSON.parse(json_params)
     if request_payload.nil? or !request_payload.has_key?('ref')
       raise "Invalid Payload"
