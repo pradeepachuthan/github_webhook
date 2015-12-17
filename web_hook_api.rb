c441242a989611b74c3b96b4f23d1f4dabd8a339
Pradeep Achuthan <pradeep.achuthan@gmail.com>
2015-12-17 16:09:17 +0530

    Adding roles in hasg

--- web_hook_api.rb
+++ web_hook_api.rb
@@ -27,7 +27,7 @@ class AutoDeployment <  Sinatra::Application
   	begin
       puts "THe branch name is", branch_name
       SETTINGS["#{branch_name}"].each{ |hash| puts "THe hash roles are #{hash}" }
-  	  SETTINGS["#{branch_name}"].each{ |hash| system ("bundle exec cap #{hash[:role]} deploy") } 
+  	  SETTINGS["#{branch_name}"].each{ |hash| system ("bundle exec cap #{hash["role"]} deploy") } 
     rescue => ex
   	  env['rack.errors'].puts ex
   	end
