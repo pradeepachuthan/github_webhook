25360861630a6f1c35bb3bfc449f932a9bbd71bf
Pradeep Achuthan <pradeep.achuthan@gmail.com>
2015-12-18 16:41:26 +0530

    Added pem path

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -13,5 +13,5 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:auth_methods] = ["publickey"]
 # it is not wise to store amazon key in repository. You need to get it from instance owner and set path to it here
-#ssh_options[:keys] = ["#{ENV['PWD']}/config/amazon_keys/zip_go_dev.pem"]
-#set :rsync_cmd, "rsync -rave 'ssh -i #{ENV['PWD']}/config/amazon_keys/zip_go_dev.pem'"
+ssh_options[:keys] = ["#{ENV['PWD']}/config/amazon_keys/zip_go_dev.pem"]
+set :rsync_cmd, "rsync -rave 'ssh -i #{ENV['PWD']}/config/amazon_keys/zip_go_dev.pem'"
